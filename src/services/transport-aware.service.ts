f2ac0cc0a38c46cd8c7a593e907e41871e8ecf71
Artavazd Barseghyan <artavazd1@gmail.com>
2025-09-02 13:11:06 +0200

    fix

--- src/services/transport-aware.service.ts
+++ src/services/transport-aware.service.ts
@@ -3,49 +3,18 @@ import { InternalServerError } from 'rest-pkg';
 import { TransportAdapterName } from '../types/transport';
 
 abstract class TransportAwareService {
-  public defaultTransport: TransportAdapterName | null = null;
   public currentTransport: TransportAdapterName | null = null;
   public activeTransportOptions: Record<string, unknown> = {};
 
-  constructor() {
-    return new Proxy(this, {
-      get: (target, prop, receiver) => {
-        const value = Reflect.get(target, prop, receiver);
-
-        if (
-          typeof value === 'function' &&
-          prop !== 'usingTransport' &&
-          prop !== 'setDefaultTransport'
-        ) {
-          return async (...methodArgs: any[]) => {
-            try {
-              return await value.apply(target, methodArgs);
-            } finally {
-              target.currentTransport = null;
-            }
-          };
-        }
-        return value;
-      },
-    });
-  }
-
-  setDefaultTransport(transport: TransportAdapterName, options: Record<string, unknown> = {}): void {
-    this.defaultTransport = transport;
-    this.activeTransportOptions = options;
-  }
-
-  usingTransport(transport: TransportAdapterName, options: Record<string, unknown> = {}): this {
+  useTransport(transport: TransportAdapterName, options: Record<string, unknown> = {}): void {
     this.currentTransport = transport;
     this.activeTransportOptions = options;
-    return this;
   }
 
   getActiveTransport(): TransportAdapterName {
-    const transportName: TransportAdapterName | null =
-      this.currentTransport ?? this.defaultTransport;
+    const transportName: TransportAdapterName | null = this.currentTransport;
     if (!transportName) {
-      throw new InternalServerError('There is no active transport registered');
+      throw new InternalServerError('There is no active transport. Set transport via useTransport() method.');
     }
     return transportName;
   }
