59052155be35deeb8bdf7ff15fa8140e8980c19d
Artavazd Barseghyan <artavazd1@gmail.com>
2025-10-29 12:47:03 +0100

    fix

--- index.ts
+++ index.ts
@@ -5,4 +5,4 @@ export { default as transportService } from './src/services/transport.service';
 export { default as TransportAwareService } from './src/services/transport-aware.service';
 export { default as TransportAdapter } from './src/transport-adapter';
 export { default as CircuitBreaker } from './src/circuit-breaker';
-export { default as BroadcastWaiter } from './src/broadcast-waiter';
+export { default as broadcastWaiter } from './src/broadcast-waiter';
